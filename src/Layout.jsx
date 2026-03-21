e182347eb16d2aaf1c0121ebcd8f14c9753fce98
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-03-21 16:29:42 +0530

    Scroll to top added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import Fotter from "./components/Fotter";
 import ContextProvider from "./context/ContextProvider";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const Layout = () => {
   return (
     <div>
       <ContextProvider>
+        <ScrollToTop />
         <Navbar />
         <Outlet />
         <Fotter />
