d417b6b845d82bf6c3377fe4aeff0ffb05b786d6
harshalpatil2809 <harshalpatil28095@gmail.com>
2026-03-22 13:11:53 +0530

    Home page Optimization

--- src/components/UI/NormalCard.jsx
+++ src/components/UI/NormalCard.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const NormalCard = ({ product}) => {
-  const navigate = useNavigate()
-  const Product = (id) =>{
-    navigate(`/product/${id}`)
-  }
+
+const NormalCard = ({ product }) => {
+  const navigate = useNavigate();
+
+  const Product = (id) => {
+    navigate(`/product/${id}`);
+  };
+
+  // 🔥 optimized image
+  const optimizedImg = `https://res.cloudinary.com/dz5yaloug/image/fetch/f_auto,q_auto,w_200,h_200,c_fill/${product.images?.[0]}`;
 
   return (
     <div className="bg-white/70 backdrop-blur-md rounded-3xl lg:px-2 md:px-2 shadow-md hover:shadow-xl transition-all duration-300 py-4 px-0 lg:w-55 md:w-55 w-45 flex flex-col items-center">
-      
+
       <img
-        src={product.images?.[0]}
+        src={optimizedImg}
         alt={product.title}
-        loading="eager"
-        className="w-32 h-auto object-cover rounded-2xl mb-4"
+        width="128"
+        height="128"
+        loading="lazy"
+        fetchPriority="auto"
+        className="w-32 h-32 object-cover rounded-2xl mb-4 cursor-pointer"
         onClick={() => Product(product.id)}
       />
 
@@ -27,7 +35,7 @@ const NormalCard = ({ product}) => {
 
       <div className="flex items-center justify-center gap-4 mt-2">
         <p className="text-xl font-bold text-green-600">
-          ₹{(product.price * 81).toFixed(2) }
+          ₹{(product.price * 81).toFixed(2)}
         </p>
 
         {product.rating && (
